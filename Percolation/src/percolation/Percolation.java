842a0b3e8e7d8c257d3b9022a9152fd79673aeb7
cscheper <cscheper@SD-CSCHEPERLP3.sd.os.local>
2013-04-06 12:42:23 -0700

    Git versioning percolation code.

---
+++ Percolation/src/percolation/Percolation.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percolation;
+
+/**
+ *
+ * @author cscheper
+ */
+public class Percolation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public Percolation(int N) {
+    } // create N-by-N grid, with all sites blocked
+    
+    public void open(int i, int j){
+    }         // open site (row i, column j) if it is not already
+    
+    public boolean isOpen(int i, int j){
+        
+        return true;
+    }    // is site (row i, column j) open?
+    
+    public boolean isFull(int i, int j){
+        return true;
+    }    // is site (row i, column j) full?
+    
+    public boolean percolates(){
+        
+        return true;
+    }            // does the system percolate?
+    
+    
+}
